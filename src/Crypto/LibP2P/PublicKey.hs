9b4042dce67b0121cb994c330b2c95a8b76780f1
David Parrish <daveparrish@tutanota.com>
2018-05-21 20:30:17 -0400

    Improve documentation for verify

--- src/Crypto/LibP2P/PublicKey.hs
+++ src/Crypto/LibP2P/PublicKey.hs
@@ -21,9 +21,10 @@ import qualified Data.ByteString                  as BS
 import           Crypto.Error                     (eitherCryptoError)
 
 class PublicKey a where
-  -- verify takes a key, message as bytes, and signature as bytes,
-  -- and returns True if the signature matches the message
-  verify :: a -> BS.ByteString -> BS.ByteString -> Either String Bool
+  verify :: a
+         -> BS.ByteString -- ^ Message
+         -> BS.ByteString -- ^ Signature
+         -> Either String Bool -- ^ Returns True if verified signature matches the message
 
 instance PublicKey Ed25519.PublicKey where
   verify pk msgb sigb = case eitherCryptoError $ Ed25519.signature sigb of
@@ -39,8 +40,7 @@ instance PublicKey Secp256k1.PubKey where
     in case mSig of
             Nothing -> Right False
             Just sig -> case mMsg of
-                             Nothing  ->
-                               Left $ "Failed to parse Secp256k1 message"
+                             Nothing  -> Left "Failed to parse Secp256k1 message"
                              Just msg -> Right $ Secp256k1.verifySig pk sig msg
 
 instance PublicKey RSA.PublicKey where
